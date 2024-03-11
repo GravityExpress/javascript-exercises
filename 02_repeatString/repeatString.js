708236a3701cc6251029b197c919dbff524326ad
GravityExpress <102402506+GravityExpress@users.noreply.github.com>
2024-03-11 16:36:18 +0800

    Completed exercise #2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(message, repetition) {
+    if (repetition < 0)
+    {
+        return "ERROR";
+    }
+    let finalMessage = "";
+    for (let i = 0; i < repetition; i++)
+    {
+        finalMessage += message;
+    }
+    return finalMessage;
 };
 
 // Do not edit below this line
