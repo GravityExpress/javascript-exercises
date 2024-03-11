7f385f02560a871baa96d47300242532844f399b
GravityExpress <102402506+GravityExpress@users.noreply.github.com>
2024-03-11 17:30:55 +0800

    Completed exercise no. 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(phrase) {
+    let splitPhrase = phrase.split("");
+    let invertedPhrase = "";
+    for (let i = -1; i >= -splitPhrase.length; i--)
+    {
+        invertedPhrase = invertedPhrase.concat(splitPhrase.at(i));
+    }
+    return invertedPhrase;
 };
 
 // Do not edit below this line
