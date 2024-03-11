8d6d29842cb87bb8e3f14c71a0059860f1e5112b
GravityExpress <102402506+GravityExpress@users.noreply.github.com>
2024-03-11 19:25:08 +0800

    Completed exercise no.4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(oldArray, ...removeElements) {
+    for (const elements of removeElements)
+    {
+        // Looped in reverse to avoid skipping elements based on an answer from: https://stackoverflow.com/a/9792947
+        for (let i = oldArray.length - 1; i >= 0; i--)
+        {
+            if (oldArray.includes(elements))
+            {
+                oldArray.splice(oldArray.indexOf(elements), 1);
+            }
+        }
+    }
+    return oldArray;
 };
 
 // Do not edit below this line
