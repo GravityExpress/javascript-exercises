576c80612a1d6e99cdb536309d5e118970f7e8cf
GravityExpress <102402506+GravityExpress@users.noreply.github.com>
2024-04-20 22:58:52 +0800

    Completed exercise no. 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    word = word.toLowerCase().replaceAll(/[!,. ]/g, '');
+    return word === Array.from(word.split("")).reverse().join('');
 };
 
 // Do not edit below this line
