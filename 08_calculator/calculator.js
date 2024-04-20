15031fbfab426ef12e2f6fc9aa894cd123902e76
GravityExpress <102402506+GravityExpress@users.noreply.github.com>
2024-04-20 21:11:20 +0800

    removed extra whitespaces

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,13 +1,13 @@
 const add = function(num1, num2) {
-	return num1 + num2;
+  return num1 + num2;
 };
 
 const subtract = function(num1, num2) {
-	return num1 - num2;
+  return num1 - num2;
 };
 
 const sum = function(arr) {
-	return arr.reduce((sum, num) => sum += num, 0);
+  return arr.reduce((sum, num) => sum += num, 0);
 };
 
 const multiply = function(arr) {
@@ -15,7 +15,7 @@ const multiply = function(arr) {
 };
 
 const power = function(num1, num2) {
-	return num1 ** num2;
+  return num1 ** num2;
 };
 
 const factorial = function(num) {
