5d9601c7b73029ca9c6bc7d35935e96f2edef493
GravityExpress <102402506+GravityExpress@users.noreply.github.com>
2024-04-21 21:24:00 +0800

    Completed exercise no. 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,28 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    let num1 = 0;
+    let num2 = 1;
+    let sequenceNum = 0;
 
+    num = +num;
+
+    if (num < 0)
+    {
+        return "OOPS";
+    }
+
+    if (num === 1)
+    {
+        return num2;
+    }
+
+    for (let i = 0; i <= num - 2; i++)
+    {
+        sequenceNum = num1 + num2;
+        num1 = num2;
+        num2 = sequenceNum;
+    }
+
+    return sequenceNum;
 };
 
 // Do not edit below this line
