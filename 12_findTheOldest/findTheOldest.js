f1146a43f1904b65d9e2ad00d2b3abd287e772c2
GravityExpress <102402506+GravityExpress@users.noreply.github.com>
2024-04-21 23:47:30 +0800

    Completed exercise no. 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,28 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    return people.reduce((oldest, person) => {
+        if (Object.keys(oldest).length === 0)
+        {
+            oldest["name"] = person.name;
+            oldest["age"] = person.yearOfDeath - person.yearOfBirth;
+        }
 
+        if (person.yearOfDeath === undefined)
+        {
+            const thisYear = (new Date()).getFullYear();
+            oldest["name"] = person.name;
+            oldest["age"] = thisYear- person.yearOfBirth;
+        }
+        else
+        {
+            if (oldest.age < (person.yearOfDeath - person.yearOfBirth))
+            {
+                oldest["name"] = person.name;
+                oldest["age"] = person.yearOfDeath - person.yearOfBirth;
+            }
+        }
+
+        return oldest;
+    }, {})
 };
 
 // Do not edit below this line
